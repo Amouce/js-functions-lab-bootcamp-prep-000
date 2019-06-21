cd700e5fc8cddd8090b56390c43592e4effb6a5f
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-21 07:45:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var name ='you',holiday ='Independence Day', days = 10
 function holidayGreeting (string){
   return ("Happy holidays!")
 }
-function happyHolidaysTo (string){
-  return (`Happy holidays", ${name},!`)
+function happyHolidaysTo (name){
+  return (`Happy holidays, ${name},!`)
 }
 function happyHolidayTo (string){
 return (`Happy ${holiday}, ${name}!`)}
