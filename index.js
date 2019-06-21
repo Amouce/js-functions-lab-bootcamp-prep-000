e77bb4979646dd923c6408bf414015b4530fc8d1
adaptable-tutorial-4108 <danielamouce@gmail.com>
2019-06-21 07:47:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ function happyHolidaysTo (string){
 }
 function happyHolidayTo (string){
 return (`Happy ${holiday}, ${name}!`)}
+
+
 function holidayCountdown(string){
   return ("It's ${days} days until ${holiday}!")}
